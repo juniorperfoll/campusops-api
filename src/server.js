713a510934894708627a7e398e41b0d0dcfc29ba
= <=>
2026-06-08 21:30:06 -0300

    Incluir lint no projeto

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
+const servidor = "teste";
+
 app.listen(port, () => {
   console.log(`CampusOps API rodando na porta ${port}`);
 });
