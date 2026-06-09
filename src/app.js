c857b61db6029a7e6916c7f9a97d52ecec5537aa
Ademar Perfoll Junior <19709@UNIDAVI.LOCAL>
2026-06-08 21:07:11 -0300

    Atualização do App.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    mensagem: "CampusOps API em execução",
+    mensagem: "CampusOps API em execução"
     disciplina: "Cloud Computing",
     tema: "DevOps",
     status: "ok"
